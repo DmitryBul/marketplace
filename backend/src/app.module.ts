1f3cba47e9b2e71d6f0bdd0eb0465e7445d62b90
DmitryBul <dmitrybulak4@gmail.com>
2025-09-03 13:20:03 +0200

    Configure app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,17 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'your_password',
-      database: 'marketplace',
-      autoLoadEntities: true,
-      synchronize: true, 
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: 5432,
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
